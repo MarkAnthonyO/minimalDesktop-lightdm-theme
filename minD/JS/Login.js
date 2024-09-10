81772a2726d9d3c2bf24daf98c57d7d179e19ced
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-09-10 11:05:12 -0600

    JQuery Update

--- minD/JS/Login.js
+++ minD/JS/Login.js
@@ -1,7 +1,9 @@
 class Login {
 	login() {
-		logger.show_message('Logged in: ' + lightdm.authentication_user + '-' + session.get_name());
-		document.getElementById('bg').style.visibility = 'hidden';
-		lightdm.start_session_sync(session.get_actual());
+		logger.show_message('Logged in: ' + lightdm.authentication_user + ' - ' + session.get_name());
+
+		$("#bg").fadeOut(800, function() {
+			setTimeout(function() {lightdm.start_session_sync(session.get_actual());}, 1000);
+		});
 	}
 }
